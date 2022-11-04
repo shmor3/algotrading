04186692d47240298735e6156c02d775177d7a3b
Robert <44666181+shmor3@users.noreply.github.com>
2022-11-03 19:32:57 -0600

    worker updates

--- workers/scripts/worker2.py
+++ workers/scripts/worker2.py
@@ -1,3 +1,23 @@
+import redis
 import time
-print('all good!')
-time.sleep(50)
+
+redisClient = redis.StrictRedis(host='10.0.0.16', port=6379, db=0)
+def get_price():
+    while(redisClient.scard('price') > 0):
+        data = str('{}'.format(redisClient.spop('price')))
+        global filtered_data
+        filtered_data = float(data.replace("b", "").translate({ord("""'"""): None}))
+    return float(filtered_data)
+def autotrade():
+    if get_price() < 30.00:
+        print('Buy')
+        autotrade()
+        time.sleep(0.2)
+    elif get_price() > 70.00:
+        print('Sell')
+        autotrade()
+        time.sleep(0.2)
+    else:
+        print(get_price())
+        autotrade()
+autotrade()
--- workers/scripts/worker3.py
+++ workers/scripts/worker3.py
@@ -1,3 +1,14 @@
+import redis
+import random
 import time
-print('all good!')
-time.sleep(50)
+
+redisClient = redis.StrictRedis(host='10.0.0.16', port=6379, db=0)
+def generate_price():
+    while True:
+        data = range(0, 101)
+        for x in data:
+            values = str(x) + '.' + str(random.uniform(0, 9))[2:5]
+            print(values)
+            redisClient.sadd('price', values)
+            time.sleep(0.2)
+generate_price()
