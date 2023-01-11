d44924e665a9e9553b67f1504fde57f5f95bb933
Robert <44666181+shmor3@users.noreply.github.com>
2023-01-11 08:54:28 -0700

    updates

--- workers/scripts/worker1.py
+++ workers/scripts/worker1.py
@@ -1,60 +1,51 @@
-import redis
-import time
 import os
+import time
+import redis
 import pyotp
+import random
 import robin_stocks.robinhood as r
-robin_username='restanford97@gmail.com'
-robin_password='57m&MDTR%fGm'
-robin_mfa='SHEP3N43QJ6W2X3S'
-tokenQuan=10
-robin_smb ='DOGE'
-localtime = time.asctime( time.localtime(time.time()) )
-redisClient = redis.Redis(host='10.0.0.16', port=6379, db=0)
-def get_price():
-    redisClient.sadd('buy_log', 0.0001)
-    while(redisClient.scard('current_price') > 0):
-        data = str('{}'.format(redisClient.spop('current_price')))
-        global filtered_data
-        filtered_data = float(data.replace("b", "").translate({ord("""'"""): None}))
-    return float(filtered_data)
-avgList = []
-def get_average():
-    data = str('{}'.format(redisClient.spop('buy_log')))
-    avgList.append(float(data.replace("b", "").translate({ord("""'"""): None})))
-    buyAvg = sum(avgList) / len(avgList)
-    return float(buyAvg)
+
+
+robin_username = ''
+robin_password = ''
+robin_mfa = ''
+localtime = time.asctime(time.localtime(time.time()))
+redisClient = redis.StrictRedis(host='10.0.0.44', port=6379, db=0)
+print('Crypto:')
+symb = str(input())
+redisClient.sadd('symb', symb)
+
 def rhauth():
     totp = pyotp.TOTP(robin_mfa).now()
-    r.login(robin_username,robin_password, store_session=False, mfa_code=totp)
-    accountInfo0 = r.load_crypto_profile(info='user_id')
-    print('User Id:', accountInfo0)
-def limitBuy():
-    r.order_buy_crypto_by_quantity(robin_smb, tokenQuan, jsonify=True)
-    print('~ buy order sent', '|', localtime, '|', 'price:', get_price(), '~')
-def limitSell():
-    r.order_sell_crypto_by_quantity(robin_smb, tokenQuan, jsonify=True)
-    print('~ sell order sent', '|', localtime, '|', 'price:', get_price(), '~')
-def autotrade():
+    print(totp)
+    r. login(robin_username, robin_password,
+             store_session=False, mfa_code=totp)
+    print('Authenticated')
+def generate_price():
     try:
-        if float(get_price()) * 10.2 < float(r.load_account_profile(info='buying_power')):
-            print('Buy @', float(get_price()) * 10)
-            limitBuy()
-            redisClient.sadd('buy_log', float(get_price()))
-            pass
-        elif float(get_price()) < float(get_average()):
-            print('Sell @', float(get_average()))
-            limitSell()
-            redisClient.sadd('sell_log', float(get_price()))
-            while(avgList.length() > 0):
-                for x in avgList:
-                    avgList.pop(0)
-            pass
-        else:
-            print(get_price())
-            pass
+        data = range(0, ++1)
+        for x in data:
+            values = str(x) + '.' + str(random.uniform(0, 9))[2:5]
+            print(localtime,':', values)
+            redisClient.sadd('current_price', values)
     except:
         pass
+
+
+def rh_markprice():
+    try:
+        y = range(0, ++1)
+        for x in y:
+            data = r.get_crypto_quote(symb, info='ask_price')
+            redisClient.sadd('current_price', data[:6])
+            print(localtime,':', data[:6])
+    except:
+        pass
+
 rhauth()
+#generate_price()
+rh_markprice()
 while True:
-    autotrade()
-    time.sleep(60)
+    rh_markprice()
+#    generate_price()
+    time.sleep(45)
--- workers/scripts/worker2.py
+++ workers/scripts/worker2.py
@@ -1,41 +1,75 @@
 import redis
-import random
 import time
-import robin_stocks.robinhood as r
+import os
 import pyotp
-symb = 'DOGE'
-robin_username='restanford97@gmail.com'
-robin_password='57m&MDTR%fGm'
-robin_mfa='SHEP3N43QJ6W2X3S'
-redisClient = redis.StrictRedis(host='10.0.0.16', port=6379, db=0)
+import robin_stocks.robinhood as r
+
+robin_username = ''
+robin_password = ''
+robin_mfa = ''
+localtime = time.asctime(time.localtime(time.time()))
+redisClient = redis.Redis(host='10.0.0.44', port=6379, db=0)
+
 def rhauth():
     totp = pyotp.TOTP(robin_mfa).now()
-    r. login(robin_username,robin_password, store_session=False, mfa_code=totp)
-    accountInfo0 = r.load_crypto_profile(info='user_id')
-    accountInfo1 = r.load_account_profile(info='buying_power')
-    print('User Id:', accountInfo0)
-# rhauth()
-def generate_price():
+    r.login(robin_username, robin_password, store_session=False, mfa_code=totp)
+    global robin_smb
+    robin_smb = str('{}'.format(redisClient.spop('symb'))).replace("b", "").translate({ord("""'"""): None})
+    global tokenQuan
+    tokenQuan = int((float(r.load_account_profile(info='buying_power')) * 0.90))
+def get_price():
+    while (redisClient.scard('current_price') > 0):
+        data = str('{}'.format(redisClient.spop('current_price')))
+        global filtered_data
+        filtered_data = float(data.replace(
+            "b", "").translate({ord("""'"""): None}))
+    return float(filtered_data)
+avgList = []
+global lastBuy
+lastBuy = ''
+def get_average():
+    while (redisClient.scard('buy_log') > 0):
+        data = str('{}'.format(redisClient.spop('buy_log')))
+        avgList.append(
+            float(data.replace("b", "").translate({ord("""'"""): None})))
+        buyAvg = sum(avgList) / len(avgList)
+        redisClient.sadd('buy_Avg', float(str(buyAvg)[:6]))
+        global lastBuy
+        lastBuy = float((str(buyAvg)[:5]))
+def limitBuy():
+    global robin_smb
+    global tokenQuan
+    r.order_buy_crypto_by_quantity(robin_smb, tokenQuan, jsonify=True)
+    print('Buy :', tokenQuan, '|', localtime)
+def limitSell():
+    global robin_smb
+    global tokenQuan
+    r.order_sell_crypto_by_quantity(robin_smb, tokenQuan, jsonify=True)
+    print('Sell:', tokenQuan, '|', localtime)
+def autotrade():
+    global lastBuy
+    global tokenQuan
     try:
-        data = range(0, ++1)
-        for x in data:
-            values = str(x) + '.' + str(random.uniform(0, 9))[2:5]
-            print(values)
-            redisClient.sadd('current_price', values)
-            time.sleep(0.1)
-    except:
-        pass
-def rh_markprice():
-    try:
-        y = range(0, ++1)
-        for x in y:
-            data = r.get_crypto_quote(symb, info='mark_price')
-            redisClient.sadd('current_price', data[:6])
-            print(data[:6])
-            time.sleep(30)
+        if (redisClient.scard('buy_Avg') == 0):
+            if True:
+                limitBuy()
+                redisClient.sadd('buy_log', float(get_price()))
+                get_average()
+                pass
+            else:
+                pass
+        elif float(r.get_crypto_quote(symb, info='bid_price')) > (lastBuy + 0.003):
+            limitSell()
+            redisClient.spop('buy_Avg')
+            while (avgList.length() > 0):
+                for x in avgList:
+                    avgList.pop(0)
+            pass
+        else:
+            pass
     except:
         pass
 rhauth()
 while True:
-    rh_markprice()
-    generate_price()
+    autotrade()
+    time.sleep(60)
